a407da405224ce63147a803e91662cd5b4eeb7b0
Valkyrie-freja <yadokame3.14@gmail.com>
2023-05-04 14:03:59 +0000

    make nauron hedder

---
+++ neuron.cpp
@@ -0,0 +1,75 @@
+#include "neuron.h"
+
+double neuron::dvdt(double _v, double _t){
+  return ( -( _v - e_leak ) + r_ext*i_ext + r_syn*i_syn - r_adp*i_adp ) / tau;
+}
+
+neuron::neuron(double _tau = 0.01){
+  // set param
+  tau = _tau;
+
+  // init variables
+  v = e_leak;
+  is_spike = false;
+  i_syn = 0.0;
+  input_syn = 0.0;
+  i_adp = 0.0;
+}
+
+bool neuron::get_spike(){
+  return is_spike;
+}
+
+void neuron::set_input_syn(double _input){
+  // _input = Σ get_spike()*weight_to_me_from_others
+  input_syn = _input;
+}
+
+void neuron::calc_i_syn(){
+  i_syn = i_syn*exp(-dt/tau) + input_syn;
+}
+
+void neuron::calc_i_adp(){
+  i_adp = i_adp*exp(-dt/tau) + is_spike;
+}
+
+void neuron::compute_v_runge_kutta(){
+  v_current = v;
+  k1 = dt*dvdt(v_current         );
+  k2 = dt*dvdt(v_current + 0.5*k1);
+  k3 = dt*dvdt(v_current + 0.5*k2);
+  k4 = dt*dvdt(v_current +     k3);
+  v = v_current + (k1 + 2*k2 + 2*k3 + k4)/6.0;
+}
+
+void neuron::set_spike(){
+  if(v > v_spike_threshold){
+    is_spike = true;
+    v = e_leak;
+    return;
+  }
+
+  is_spike = false;
+  return;
+}
+
+void neuron::compute_process(){
+  // 自分に接続している相手のスパイク確認
+  // input_syn = Σ spike * weight を計算
+  // set_input_syn()
+  //   get_spike()
+  //   get_weight()
+  
+  calc_i_syn();
+  calc_i_adp();
+
+  // compute v from dvdt and runge-kutta approximate
+  compute_v_runge_kutta();
+
+  // spike チェック
+  set_spike();
+}
+
+void neuron::set_dt(double _dt){
+  dt = _dt;
+}
---
+++ neuron.h
@@ -0,0 +1,41 @@
+#ifndef NEURON_H_
+#define NEURON_H_
+
+class neuron{
+  private:
+    double v;
+    const double e_leak = -65.0;
+    const double r_ext = 1;
+    const double i_ext = 20.0;
+  public: const double tau;
+  private:  
+    bool is_spike;
+    // receive spike
+    const double r_syn = 1;
+    double i_syn;
+    double input_syn;
+    // send spike
+    const double r_adp = 0.5;
+    double i_adp;
+
+    double dvdt(double _v, double _t);
+
+  public:
+    neuron(double _tau);
+    bool get_spike();
+    void set_input_syn(double _input);
+
+  private:
+    void calc_i_syn();
+    void calc_i_adp();
+    void compute_v_runge_kutta();
+    void set_spike();
+
+  public:
+    void compute_process();
+
+  private: double dt;
+  public:  void set_dt(double _dt);
+};
+
+#endif// NEURON_H_
